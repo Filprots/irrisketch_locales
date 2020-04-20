84a86f347057d9b0e8f895802611231212dbdb38
Filiph Protsenko <filprocenko@gmail.com>
2020-04-20 19:30:41 +0300

    Added pages and loading process

---
+++ catalogs/.loading/loading.js
@@ -0,0 +1,30 @@
+var lang = {
+    en: {
+        loading: "Initializing structure",
+        interactive: "Downloading libraries",
+        DOMContentLoaded: "Recieving media",
+        complete: "Loading project",
+        windowLoaded: "Preparing scene",
+        loadingObjects: "Loading objects",
+        loadingLayouts: "Preparing layouts",
+        pdfConverting: "Converting PDF"
+    }, ru: {
+        loading: "Инициализация структуры",
+        interactive: "Скачивание библиотек",
+        DOMContentLoaded: "Загрузка графики",
+        complete: "Подготовка проекта",
+        windowLoaded: "Подготовка сцены",
+        loadingObjects: "Создание объектов",
+        loadingLayouts: "Отрисовка чертежей",
+        pdfConverting: "Конвертация PDF"
+    }, ua: {
+        loading: "Инициализация структуры",
+        interactive: "Скачивание библиотек",
+        DOMContentLoaded: "Загрузка графики",
+        complete: "Подготовка проекта",
+        windowLoaded: "Подготовка сцены",
+        loadingObjects: "Создание объектов",
+        loadingLayouts: "Отрисовка чертежей",
+        pdfConverting: "Конвертация PDF"
+    }
+};

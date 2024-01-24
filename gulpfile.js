2f493485ed27b5bacecf9a8f08af873847e20982
Fil Prots <filprocenko@gmail.com>
2024-01-24 12:03:03 +0100

    added gulp strings checker

--- gulpfile.js
+++ gulpfile.js
@@ -47,6 +47,70 @@ gulp.task('check-json-lines-int', function () {
         }));
 });
 
+gulp.task('check-json-lines-calc', function () {
+    return gulp.src('irricalc/**/*.json') // Adjust the path to your JSON files
+        .pipe(through2.obj(function (file, _, cb) {
+            if (file.isBuffer()) {
+                const lines = file.contents.toString().split('\n').length;
+                console.log(`${file.relative}: ${lines} lines`);
+            }
+            cb(null, file);
+        }));
+});
 
+gulp.task('check-json-lines-admin', function () {
+    return gulp.src('irrisadmin/**/*.json') // Adjust the path to your JSON files
+        .pipe(through2.obj(function (file, _, cb) {
+            if (file.isBuffer()) {
+                const lines = file.contents.toString().split('\n').length;
+                console.log(`${file.relative}: ${lines} lines`);
+            }
+            cb(null, file);
+        }));
+});
+
+gulp.task('check-json-lines-seller', function () {
+    return gulp.src('irriseller/**/*.json') // Adjust the path to your JSON files
+        .pipe(through2.obj(function (file, _, cb) {
+            if (file.isBuffer()) {
+                const lines = file.contents.toString().split('\n').length;
+                console.log(`${file.relative}: ${lines} lines`);
+            }
+            cb(null, file);
+        }));
+});
+
+gulp.task('check-json-lines-price', function () {
+    return gulp.src('irrisprice/**/*.json') // Adjust the path to your JSON files
+        .pipe(through2.obj(function (file, _, cb) {
+            if (file.isBuffer()) {
+                const lines = file.contents.toString().split('\n').length;
+                console.log(`${file.relative}: ${lines} lines`);
+            }
+            cb(null, file);
+        }));
+});
+
+gulp.task('check-json-lines-planner', function () {
+    return gulp.src('planner/**/*.js') // Adjust the path to your JSON files
+        .pipe(through2.obj(function (file, _, cb) {
+            if (file.isBuffer()) {
+                const lines = file.contents.toString().split('\n').length;
+                console.log(`${file.relative}: ${lines} lines`);
+            }
+            cb(null, file);
+        }));
+});
+
+gulp.task('check-json-lines-planner_emb', function () {
+    return gulp.src('planner_embedded/**/*.js') // Adjust the path to your JSON files
+        .pipe(through2.obj(function (file, _, cb) {
+            if (file.isBuffer()) {
+                const lines = file.contents.toString().split('\n').length;
+                console.log(`${file.relative}: ${lines} lines`);
+            }
+            cb(null, file);
+        }));
+});
 
 // gulp.task('buildTest', gulp.series());
